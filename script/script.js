b9bb12090d6f9ee8053853f9937eba1e154d029d
n-desantis92 <dsnwebdev@gmail.com>
2021-04-06 11:22:27 +0200

    aggiunto metodo cerca

--- script/script.js
+++ script/script.js
@@ -79,15 +79,99 @@ var app = new Vue ({
                     date: '10/01/2020 15:50:00',
                     message: 'Si, ma preferirei andare al cinema',
                     status: 'received'
+                },
+
+            ],
+        },
+        {
+            name: 'gianny',
+            avatar: '_5',
+            visible: true,
+            messages: [{
+                date: '28/03/2020 10:10:40',
+                message: 'hey ci sei?',
+                status: 'received'
+            },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'Ah scusa!',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Ivan',
+            avatar: '_6',
+            visible: true,
+            messages: [{
+                date: '28/03/2020 10:10:40',
+                message: 'che facevi oggi al centro?',
+                status: 'received'
+            },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'al centro dove?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'non eri tu?',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Mark',
+            avatar: '_7',
+            visible: true,
+            messages: [{
+                date: '28/03/2020 10:10:40',
+                message: 'Ho perso tutto!',
+                status: 'received'
+            },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicuro?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'sicurissimo!',
+                    status: 'received'
+                }
+            ],
+        },
+        {
+            name: 'Jennifer',
+            avatar: '_8',
+            visible: true,
+            messages: [{
+                date: '28/03/2020 10:10:40',
+                message: 'La gallina fa le uova',
+                status: 'received'
+            },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicura di star bene?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'non lo so!',
+                    status: 'received'
                 }
             ],
         },
     ],
     newmess: "",
     indexcont: 0,
-    giorno: dayjs().date(),
-    mese: dayjs().month(),
-    anno: dayjs().year(),
+    ricerca: "",
+    contattifiltrati: ""
   },
 
 
@@ -98,10 +182,10 @@ var app = new Vue ({
       if (this.newmess !== "") {
 
         const nuovomess = {
-                            date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
-                            message: this.newmess,
-                            status: 'sent'
-                        };
+              date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+              message: this.newmess,
+              status: 'sent'
+          };
 
         this.contacts[this.indexcont].messages.push(nuovomess);
         this.newmess = "";
@@ -119,10 +203,33 @@ var app = new Vue ({
 
     },
 
-
+    // filtraContatti: function () {
+    //   this.contacts.filter(this.ricerca);
+    // },
 
     seleziona: function (i) {
       this.indexcont = i;
-    }
+    },
+
+    cerca: function () {
+      this.contacts.forEach((item, i) => {
+        console.log(item.name,i);
+        if (this.ricerca != "") {
+
+          if (item.name.includes(this.ricerca)) {
+            console.log("ce l'ho fatta");
+            this.visibile = "vis";
+            console.log(this.visibile);
+
+            return this.indexcont = i;
+
+          }else {
+            this.visibile = "notvis";
+            console.log(this.visibile);
+            return false
+          }
+        }
+      });
+    },
   }
 })
